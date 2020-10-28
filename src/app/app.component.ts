b2f88eafe45f6c322e7e01b0050b1300b70d05b4
Томилов Иван <toothless1234321@gmail.com>
2020-10-28 14:13:41 +0300

    init 8 changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'homework2';
+
+  firstOperand: number;
+  secondOperand: number;
+  operator1: string;
+  operator2: string;
+  result: boolean;
+  errorMessage: string;
+
+  doCalc(){
+      if(this.firstOperand != null && this.secondOperand != null  && this.operator2 != null){
+        if(this.operator1!='!'){
+          if(this.operator2=='&&'){
+            this.result = Boolean(this.firstOperand && this.secondOperand)
+            this.errorMessage = 'Нет ошибок'
+          }else if(this.operator2=='||'){
+            this.result = Boolean(this.firstOperand || this.secondOperand)
+            this.errorMessage = 'Нет ошибок'
+          }
+        }else if (this.operator1 =='!'){
+          if(this.operator2=='&&'){
+            this.result = !Boolean(this.firstOperand && this.secondOperand)
+            this.errorMessage = 'Нет ошибок'
+          }else if(this.operator2=='||'){
+            this.result = !Boolean(this.firstOperand || this.secondOperand)
+            this.errorMessage = 'Нет ошибок'
+          }
+        }
+      }else if(this.firstOperand==null){
+        this.errorMessage='Нет первого операнда!'
+      }else if(this.secondOperand==null){
+        this.errorMessage='Нет второго операнда!'
+      }else if(this.operator2==null){
+        this.errorMessage='Нет оператора действия!'
+      }
+    }
 }
